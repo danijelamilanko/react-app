1d8fb2e9e2ac1e8074f774f0094e52c35501179c
Danijela Milanko <ddmilanko@gmail.com>
2018-08-18 16:54:15 +0200

    continue

--- src/components/navigation/TopNavigation.js
+++ src/components/navigation/TopNavigation.js
@@ -1,9 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Menu, Dropdown, Image } from "semantic-ui-react";
+import { Menu, Dropdown } from "semantic-ui-react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import gravatarUrl from "gravatar-url";
 import * as actions from "../../actions/auth";
 
 const TopNavigation = ({user, logout}) => (
@@ -13,7 +12,7 @@ const TopNavigation = ({user, logout}) => (
         </Menu.Item>
 
         <Menu.Menu position="right">
-            <Dropdown trigger={<Image avatar src={gravatarUrl(user.email)}/>}>
+            <Dropdown trigger={user.email}>
                 <Dropdown.Menu>
                     <Dropdown.Item onClick={() => logout()}>Logout</Dropdown.Item>
                 </Dropdown.Menu>
